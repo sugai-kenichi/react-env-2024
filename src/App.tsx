589904735b8f200284ab400e86f9ddc18f550b2a
sugai <kinspe0401@gmail.com>
2025-03-05 16:37:10 +0900

    mod

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>Vite + React + Onemore</h1>
+            <h1 className='text-3xl text-red-500'>Vite + React + Onemore</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
